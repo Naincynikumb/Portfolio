f0a2f5c4a3c43342902f3ff5af369025ef1ada23
Naincynikumb <84220087+Naincynikumb@users.noreply.github.com>
2024-09-26 18:23:08 +0530

    Add files via upload

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAat4k8sTx17ZleEwvLdKejKex-poV_aCE",
+  authDomain: "portfolio-5b606.firebaseapp.com",
+  projectId: "portfolio-5b606",
+  storageBucket: "portfolio-5b606.appspot.com",
+  messagingSenderId: "646956875752",
+  appId: "1:646956875752:web:53716b1d31adc97b172e5d",
+  measurementId: "G-VEB8CJQJZZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
